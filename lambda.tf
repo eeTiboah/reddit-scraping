e0193ab60f058c8d79f0bbd276dc052382cedd22
eeTiboah <etiboah@gmail.com>
2023-01-09 14:31:03 +0000

    remove created iam role

--- lambda.tf
+++ lambda.tf
@@ -1,23 +1,5 @@
 
 
-resource "aws_iam_role" "serverless_role_lambda" {
-  name               = "serverless_role"
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-      "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-POLICY
-}
 
 module "lambda_function" {
   source = "terraform-aws-modules/lambda/aws"
@@ -26,7 +8,6 @@ module "lambda_function" {
   description   = "Scrape reddit data"
   handler       = "main.handler"
   runtime       = "python3.8"
-  lambda_role = aws_iam_role.serverless_role_lambda.arn
   attach_cloudwatch_logs_policy           = false
 
   source_path = "./src/lambda-function"

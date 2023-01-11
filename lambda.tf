d17665f08dbef82003ab9ccd305112b61a3b2789
eeTiboah <etiboah@gmail.com>
2023-01-11 10:36:42 +0000

    add s3 bucket

--- lambda.tf
+++ lambda.tf
@@ -1,5 +1,10 @@
 
 
+resource "aws_s3_bucket" "bucket" {
+  bucket        = "gitTerrabucket-100"
+  force_destroy = true
+}
+
 module "lambda_function" {
   source = "terraform-aws-modules/lambda/aws"
   version       = "~>4.0"
@@ -15,6 +20,7 @@ module "lambda_function" {
     module.lambda_layer_s3.lambda_layer_arn
   ]
   store_on_s3 = true
+  s3_bucket   = aws_s3_bucket.bucket.id
   # timeout     = 120
   # memory_size = 128
 
@@ -33,10 +39,10 @@ module "lambda_layer_s3" {
 
   create_layer = true
   store_on_s3 = true
+  s3_bucket   = aws_s3_bucket.bucket.id
 
   layer_name          = "lambda-layer-s3"
   description         = "Lambda layer for the lambda function"
-  compatible_runtimes = ["python3.9"]
   runtime = "python3.9"
 
   source_path = [{

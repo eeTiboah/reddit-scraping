54a5680470b48d3d49a3210b17410f923ac34459
eeTiboah <etiboah@gmail.com>
2023-01-14 01:54:07 +0000

    edit s3 backend

--- backend.tf
+++ backend.tf
@@ -1,9 +1,14 @@
 
 
 terraform {
-  backend "s3" {
-    bucket = "github-openid-terraform-1000"
-    region = "us-east-1"
-    key = "global/s3/terraform.tfstate"
-  }
+  backend "s3" {}
 }
+
+
+# terraform {
+#   backend "s3" {
+#     bucket = "github-openid-terraform-1000"
+#     region = "us-east-1"
+#     key = "global/s3/terraform.tfstate"
+#   }
+# }

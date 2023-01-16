73794c9be6a4250cb4a61ab3dfaac2150e1f6399
eeTiboah <etiboah@gmail.com>
2023-01-16 13:47:09 +0000

    rename precommit file

--- backend.tf
+++ backend.tf
@@ -5,10 +5,10 @@ terraform {
 }
 
 
-# terraform {
-#   backend "s3" {
-#     bucket = "github-openid-terraform-1000"
-#     region = "us-east-1"
-#     key = "global/s3/terraform.tfstate"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = "github-openid-terraform-1000"
+    region = "us-east-1"
+    key = "global/s3/terraform.tfstate"
+  }
+}

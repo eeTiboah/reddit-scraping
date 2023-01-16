6c3418a4c932a1a341ab6d1bfb4c67187d6a728f
eeTiboah <etiboah@gmail.com>
2023-01-16 17:27:16 +0000

    added output

--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,8 @@ output "lambda_arn" {
   value       = module.lambda_function.lambda_function_arn
   description = "Lambda function ARN"
 }
+
+output "lambda_arn" {
+  value       = module.lambda_function.lambda_function_url
+  description = "Lambda function URL"
+}

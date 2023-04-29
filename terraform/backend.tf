5bf1c8afd28fd73c9cd2f71b6ffd5a27302e3912
Emmanuel Tiboah <etiboah@gmail.com>
2023-04-29 14:28:28 +0000

    comment out dynamodb table in backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -5,6 +5,6 @@ terraform {
     bucket = "aws-data-ingestion-project-state"
     region = "eu-central-1"
     key    = "global/s3/terraform.tfstate"
-    dynamodb_table = "aws-ingestion-dynamodb"
+    # dynamodb_table = "aws-ingestion-dynamodb"
   }
 }
